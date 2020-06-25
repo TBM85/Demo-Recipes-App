0f7e5c808de6be6fe3d2a347b14b7d72b089df13
TBM85 <tballesterm@gmail.com>
2020-06-25 19:26:17 +0200

    Adding content to the Recipes Component

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe Number 1',
+      'This is a simply a test',
+      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_1280.jpg'
+    ),
+    new Recipe(
+      'Test Recipe Number 2',
+      'This is a simply a test',
+      'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_1280.jpg'
+    ),
+    new Recipe(
+      'Test Recipe Number 3',
+      'This is a simply a test',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'
+    ),
+    new Recipe(
+      'Test Recipe Number 4',
+      'This is a simply a test',
+      'https://cdn.pixabay.com/photo/2017/12/23/15/54/food-3035432_1280.jpg'
+    )
+  ];
 
   constructor() { }
 
