71b251bcd2041f06438a1a3c9a916726e2a3b9a2
TBM85 <tballesterm@gmail.com>
2020-07-01 10:42:07 -0500

    Adding ingredients to recipe details

--- src/app/components/recipes/recipes.service.ts
+++ src/app/components/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,38 @@ export class RecipeService {
     new Recipe(
       'Test Recipe Number 1',
       'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_1280.jpg'
+      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_1280.jpg',
+      [
+        new Ingredient('Flour', 4),
+        new Ingredient('Cheese', 1)
+      ]
     ),
     new Recipe(
       'Test Recipe Number 2',
       'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_1280.jpg'
+      'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_1280.jpg',
+      [
+        new Ingredient('Eggs', 12),
+        new Ingredient('Jam', 3)
+      ]
     ),
     new Recipe(
       'Test Recipe Number 3',
       'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
+      [
+        new Ingredient('Rice', 2),
+        new Ingredient('Mushrooms', 20)
+      ]
     ),
     new Recipe(
       'Test Recipe Number 4',
       'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2017/12/23/15/54/food-3035432_1280.jpg'
+      'https://cdn.pixabay.com/photo/2017/12/23/15/54/food-3035432_1280.jpg',
+      [
+        new Ingredient('Salt', 1),
+        new Ingredient('Peanut', 10)
+      ]
     )
   ];
 
