d5192888143c0077c39315f7238bea0d79623380
TBM85 <tballesterm@gmail.com>
2020-07-01 08:58:20 -0500

    Managing recipes in a Recipe Service

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,34 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe Number 1',
-      'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_1280.jpg'
-    ),
-    new Recipe(
-      'Test Recipe Number 2',
-      'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_1280.jpg'
-    ),
-    new Recipe(
-      'Test Recipe Number 3',
-      'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'
-    ),
-    new Recipe(
-      'Test Recipe Number 4',
-      'This is a simply a test',
-      'https://cdn.pixabay.com/photo/2017/12/23/15/54/food-3035432_1280.jpg'
-    )
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
