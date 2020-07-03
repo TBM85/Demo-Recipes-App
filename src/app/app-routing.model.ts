13f83f1515e9d3c45768169007b94a964256be3e
TBM85 <tballesterm@gmail.com>
2020-07-03 11:13:55 -0500

    Retrieving route parameters

--- src/app/app-routing.model.ts
+++ src/app/app-routing.model.ts
@@ -7,13 +7,14 @@ import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-s
 import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent},
+    { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent}
+    { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/edit', component: RecipeEditComponent }
   ] },
-  { path: 'shopping-list', component: ShoppingListComponent}
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
