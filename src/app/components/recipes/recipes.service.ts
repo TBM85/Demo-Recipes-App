6476d869876e8d1cdaec3ef639b73189639fcd1a
TBM85 <tballesterm@gmail.com>
2020-08-24 13:29:11 -0400

    Some changes in the content. The image of each item in the list of recipes is deleted

--- src/app/components/recipes/recipes.service.ts
+++ src/app/components/recipes/recipes.service.ts
@@ -10,39 +10,56 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe Number 1',
-      'This is a simply a test',
+      'Strawberry Cheesecake',
+      'Classic strawberry cheesecake with the easiest recipe, perfect for everyday baking',
       'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_1280.jpg',
       [
-        new Ingredient('Flour', 4),
-        new Ingredient('Cheese', 1)
+        new Ingredient('Cream cheese', 1),
+        new Ingredient('Biscuits', 12),
+        new Ingredient('Punnet of strawberries', 1),
+        new Ingredient('Icing sugar', 2),
+        new Ingredient('Butter', 1),
+        new Ingredient('Vanilla pod', 1)
       ]
     ),
     new Recipe(
-      'Test Recipe Number 2',
-      'This is a simply a test',
+      'Tomato Soup',
+      'A tasty tomato soup made with natural tomatoes, a quick recipe for any day',
       'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_1280.jpg',
       [
-        new Ingredient('Eggs', 12),
-        new Ingredient('Jam', 3)
+        new Ingredient('Tomatoes', 6),
+        new Ingredient('Chicken broth', 3),
+        new Ingredient('Onion', 1),
+        new Ingredient('Garlic', 3),
+        new Ingredient('Basil leaves', 1),
+        new Ingredient('Thyme', 1)
       ]
     ),
     new Recipe(
-      'Test Recipe Number 3',
-      'This is a simply a test',
+      'Beef with sauteed vegetables',
+      'This beef with sauteed vegetables is a quick and easy way to get some veggies in the diet',
       'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
       [
-        new Ingredient('Rice', 2),
-        new Ingredient('Mushrooms', 20)
+        new Ingredient('Beef', 5),
+        new Ingredient('Mushrooms', 20),
+        new Ingredient('Peppers', 10),
+        new Ingredient('Carrots', 2),
+        new Ingredient('Lettuce', 1),
+        new Ingredient('Soy Sauce', 1)
       ]
     ),
     new Recipe(
-      'Test Recipe Number 4',
-      'This is a simply a test',
+      'Chicken Burger',
+      'Delicious and healthy homemade chicken burgers, ideal for the little ones in the house',
       'https://cdn.pixabay.com/photo/2017/12/23/15/54/food-3035432_1280.jpg',
       [
-        new Ingredient('Salt', 1),
-        new Ingredient('Peanut', 10)
+        new Ingredient('Chicken', 1),
+        new Ingredient('Brioche buns', 1),
+        new Ingredient('Cherry tomato', 12),
+        new Ingredient('Red onion', 1),
+        new Ingredient('Paprika', 1),
+        new Ingredient('Black pepper', 1),
+        new Ingredient('Basil leaves', 2)
       ]
     )
   ];
